7529289f5cca3056a069fd6f8374b6b73007c60e
Misra Turp <misraturp@gmail.com>
2019-10-29 16:42:45 +0100

    reading from JSON instead of file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import CardList from './CardList';
-import {robots} from './robots';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
 
 
@@ -9,10 +9,16 @@ class App extends React.Component {
 	constructor(){
 		super()
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: ''
 
 		}
+	}
+
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response=> response.json())
+		.then(users=>this.setState({robots:users}));
 	}
 
 	onSearchChange = (event) => {
@@ -21,14 +27,17 @@ class App extends React.Component {
 
 
 	render(){
-		const filteredRobots = this.state.robots.filter(robot => {
-			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		const {robots, searchfield} = this.state;
+		const filteredRobots = robots.filter(robot => {
+			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
 		})
 		return (
 			<div className='tc'>
 				<h1 className='f1'> RoboFriends </h1>
 				<SearchBox searchChange={this.onSearchChange}/>
-				<CardList robots={filteredRobots}/>
+				<Scroll>
+					<CardList robots={filteredRobots}/>
+				</Scroll>
 			</div>
 			)
 		}
