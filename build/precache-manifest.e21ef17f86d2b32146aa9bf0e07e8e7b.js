cd0c2676abff3d3e1f13ef213eda5e6b21620a1e
Misra Turp <misraturp@gmail.com>
2019-11-05 11:57:30 +0100

    git pages set up

---
+++ build/precache-manifest.e21ef17f86d2b32146aa9bf0e07e8e7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7805ec13af91ebece9bef81713fcbae",
+    "url": "/robofriends_app/index.html"
+  },
+  {
+    "revision": "2a7df483b8979bcb2ab4",
+    "url": "/robofriends_app/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "79b524784d1e807d45f2",
+    "url": "/robofriends_app/static/css/main.b6abb149.chunk.css"
+  },
+  {
+    "revision": "2a7df483b8979bcb2ab4",
+    "url": "/robofriends_app/static/js/2.3cd86ebb.chunk.js"
+  },
+  {
+    "revision": "79b524784d1e807d45f2",
+    "url": "/robofriends_app/static/js/main.2e158cc4.chunk.js"
+  },
+  {
+    "revision": "e5b7f6af2039c7052f6f",
+    "url": "/robofriends_app/static/js/runtime-main.ab81f92f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends_app/static/media/SEGA.12a3db8b.woff"
+  }
+]);
