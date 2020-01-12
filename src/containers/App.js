afcf3dfa58eba839c71bca454ea4c9b0c0608b65
Misra Turp <misraturp@gmail.com>
2020-01-12 23:20:22 +0100

    redux complete

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,35 +6,45 @@ import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
-import { setSearchField } from '../actions'
+import { setSearchField, requestRobots } from '../actions'
 
 const mapStateToProps = state => {
 	return {
-		searchField: state.searchRobots.searchField
+		searchField: state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		isPending: state.requestRobots.isPending,
+		error: state.requestRobots.error
 	}
 }
 
 const mapDispatchToProps = (dispatch) => {
 	return {
-		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestrobots: () => dispatch(requestRobots()) // same as this: requestRobots(dispatch)
+		
 	}
 }
 
 
 class App extends React.Component {
-	constructor(){
-		super()
-		this.state = {
-			robots: [],
-			// searchfield: ''
 
-		}
-	}
+	// change after thunk
+	// constructor(){
+	// 	super()
+	// 	this.state = {
+	// 		robots: [],
+	// 		// searchfield: ''
+
+		// }
+	// }
 
 	componentDidMount(){
-		fetch('https://jsonplaceholder.typicode.com/users')
-		.then(response=> response.json())
-		.then(users=>this.setState({robots:users}));
+		this.props.onRequestrobots();
+
+	// change after thunk
+	// 	fetch('https://jsonplaceholder.typicode.com/users')
+	// 	.then(response=> response.json())
+	// 	.then(users=>this.setState({robots:users}));
 	}
 
 	// onSearchChange = (event) => {
@@ -44,12 +54,14 @@ class App extends React.Component {
 
 	render(){
 		// const {robots, searchfield} = this.state;
-		const {robots} = this.state;
-		const { searchField, onSearchChange } = this.props;
+		// const {robots} = this.state;
+		const { searchField, onSearchChange, robots, isPending } = this.props;
 		const filteredRobots = robots.filter(robot => {
 			return robot.name.toLowerCase().includes(searchField.toLowerCase());
 		})
-		return (
+		return isPending ?
+			<h1> Loading </h1> :
+			(
 			<div className='tc'>
 				<h1 className='f1'> RoboFriends </h1>
 				<SearchBox searchChange={onSearchChange}/>
